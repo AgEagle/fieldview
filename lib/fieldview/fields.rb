4c3eb3dc43cafaaa0d45ab2551952b8665958945
Paul Susmarski <Paul.susmarski@gmail.com>
2017-04-12 16:49:42 -0600

    Update fields to look cleaner and support limits

--- lib/fieldview/fields.rb
+++ lib/fieldview/fields.rb
@@ -1,28 +1,35 @@
 module FieldView
   class Fields
-    def self.list(auth_token, next_token=nil)
-      response = auth_token.execute_request(:get, "fields",
+    PATH = "fields"
+    def self.list(auth_token, limit: nil, next_token: nil)
+      limit ||= FieldView.default_page_limit
+      response = auth_token.execute_request(:get, PATH,
         headers: {
-          FieldView::NEXT_TOKEN_HEADER_KEY => next_token
+          FieldView::NEXT_TOKEN_HEADER_KEY => next_token,
+          FieldView::PAGE_LIMIT_HEADER_KEY => limit
           })
-      
       next_token = response.http_headers[FieldView::NEXT_TOKEN_HEADER_KEY]
-      last_response = response.http_status
 
-      return_data = nil
-      case response.http_status
-      # Partial result, will have more data
-      when 206
+      byebug
+      if (response.http_status == 200 || response.http_status == 206) then
+        # 206: Partial result, will have more data
+        # 200: When all the results were in the list
         return_data = response.data[:results]
-      # Nothing modified since last request
-      when 304
+      elsif (response.http_status == 304)
+        # 304: Nothing modified since last request
         return_data = []
-      # When all the results were in the list
-      when 200
-        return_data = response.data[:results]
+      else
+        # This should never happen
+        return_data = nil
       end
-
-      return return_data.collect { |i| Field.new(i, self.auth_token) }
+      
+      return ListObject.new(
+        self,
+        auth_token,
+        return_data.collect { |i| Field.new(i, auth_token) },
+        response.http_status,
+        next_token: next_token,
+        limit: limit)
     end
   end
 end
--- lib/fieldview/list_object.rb
+++ lib/fieldview/list_object.rb
@@ -1,9 +1,16 @@
 module FieldView
   class ListObject
+    attr_accessor :limit
+    attr_reader :auth_token, :data, :last_http_status, :next_token
     include Enumerable
 
-    def initialize(auth_token, data, http_status, next_token: nil)
-      super
+    def initialize(listable, auth_token, data, http_status, next_token: nil, limit: 100)
+      @listable = listable
+      @auth_token = auth_token
+      @data = data
+      @last_http_status = http_status
+      @next_token = next_token
+      @limit = 100
     end
 
     def each(&blk)
